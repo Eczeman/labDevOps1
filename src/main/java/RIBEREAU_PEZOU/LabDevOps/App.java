0c069eb466dca469da19a63b5df9aeec89ec6a4b
Eczeman <nribereau@gmail.com>
2017-10-06 15:57:47 +0200

    Début partie Jenkins

---
+++ src/main/java/RIBEREAU_PEZOU/LabDevOps/App.java
@@ -0,0 +1,15 @@
+package RIBEREAU_PEZOU.LabDevOps;
+
+/**
+ * Hello world!
+ * @version 1.0
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	//Bonjour le monde
+        System.out.println( "Hello World!" );
+    }
+}
